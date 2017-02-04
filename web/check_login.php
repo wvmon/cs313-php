61393f57fd002345ce3b4984a2f37393f657e95f
William Montesedoca <wvmon360@gmail.com>
2017-02-04 14:53:11 -0700

    restore

--- web/check_login.php
+++ web/check_login.php
@@ -1,21 +1,12 @@
-!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
+ <title>Student Budget</title>
  <meta charset="utf-8">
  <meta name="viewport" content="widtd=device-widtd, initial-scale=1">
  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
- <script>
- function check_empty() {
-    if (document.getElementById('priority').value == "" || document.getElementById('title').value == "" || document.getElementById('price').value == "" || document.getElementById('releasedate').value == "" || document.getElementById('rating').value == "") {
-        alert("Please fill out all fields.");
-    } else {
-        document.getElementById('form').submit();
-        alert("Form Submitted Successfully.");
-    }
-}
-</script>
 </head>
 <body>
     <div class="row headerBox">
@@ -35,13 +26,13 @@
             $dbName = ltrim($dbopts["path"],'/');
             if ($_SERVER['REQUEST_METHOD'] == "POST") {
                 try {
-                    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
                     $username = $_POST['username'];
                     $password = $_POST['password'];
+                    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
                     $q = "SELECT password FROM users WHERE username='".$username."'";
                     foreach ($db->query($q) as $row) {
                         if (password_verify($password, $row['password'])) {
-                            $_SESSION['loggedin'] = $username;
+                            $_SESSION['check_login'] = $username;
                         } else {
                             $_SESSION['error'] = "Invalid credentials";
                             header("Location: login.php");
@@ -54,14 +45,14 @@
                     die();
                 }
             }
-            echo '<h3 class="headerText2" style="float:right;">Welcome '. $_SESSION['loggedin'] . '!</h3>';
-            if (!isset($_SESSION['loggedin'])) {
+            echo '<h3 class="headerText2" style="float:right;">Welcome '. $_SESSION['check_login'] . '!</h3>';
+            if (!isset($_SESSION['check_login'])) {
                 $_SESSION['error'] = "Invalid credentials";
                 header("Location: login.php");
                 exit;
             }
             ?>
+            <p>Hello!</p>
         </div>
     </div>
 </body>
-</html>
