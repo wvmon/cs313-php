45383839c3c09a790f4d42aa924e7b69f28cff34
William Montesedoca <wvmon360@gmail.com>
2017-02-04 18:32:28 -0700

    let's see

---
+++ web/team/dbConnect.php
@@ -0,0 +1,29 @@
+<?php
+function get_db() {
+	$db = NULL;
+	try {
+		$dbUrl = getenv('DATABASE_URL');
+		if (!isset($dbUrl) || empty($dbUrl)) {
+			$dbUrl = "postgres://postgres:lz3f445r@localhost:5432/cs313db";
+		}
+		// Get the various parts of the DB Connection from the URL
+		$dbopts = parse_url($dbUrl);
+		$dbHost = $dbopts["host"];
+		$dbPort = $dbopts["port"];
+		$dbUser = $dbopts["user"];
+		$dbPassword = $dbopts["pass"];
+		$dbName = ltrim($dbopts["path"],'/');
+		// Create the PDO connection
+		$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+		// this line makes PDO give us an exception when there are problems, and can be very helpful in debugging!
+		$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+	}
+	catch (PDOException $ex) {
+		// If this were in production, you would not want to echo
+		// the details of the exception.
+		echo "Error connecting to DB. Details: $ex";
+		die();
+	}
+	return $db;
+}
+?>
