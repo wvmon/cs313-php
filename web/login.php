761af22989e7e36961b2617008158ce60ae84b20
William Montesedoca <wvmon360@gmail.com>
2017-02-04 18:00:37 -0700

    OMG

--- web/login.php
+++ web/login.php
@@ -18,18 +18,29 @@ if (isset($_SESSION['loggedin'])) {
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script>
+            function check_empty() {
+                if (document.getElementById('username').value == "" || document.getElementById('password').value == "" ) {
+                    //alert("Please fill out all fields.");
+                    document.getElementById("error").textContent= "Empty fields exist.";
+                    return false;
+                } else {
+                    document.getElementById('form').submit();
+                }
+            }
+        </script>
     </head>
     <body>
         <div class="container">
             <div class="row">
                 <div class="col-lg-4"></div>
                 <div class="col-lg-4">
-                    <form action="loggedin.php" method="POST">
+                    <form onsubmit="return check_submit()" action="loggedin.php" method="POST">
                         <fieldset>
                             <legend class="legend">Login</legend>
                             <div class="form_stuff">
-                                <input type="text" name="username" class="login" placeholder="Username"><br><br>
-                                <input type="password" name="password" class="login" placeholder="Password"><br><br>
+                                <input type="text" id="username" name="username" class="login" placeholder="Username"><br><br>
+                                <input type="password" id="password" name="password" class="login" placeholder="Password"><br><br>
                                 <input type="submit" name="login" class="btn" value="Login"><br><br>
                                 <a href="signup.php">Register for an account</a>
                             </div>                                
