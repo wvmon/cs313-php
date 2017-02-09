bd1f11d212aed8dcb5d6206589b72078e713b6e8
William Montesedoca <wvmon360@gmail.com>
2017-02-08 19:30:04 -0700

    Fixed execute method in php

--- web/account_created.php
+++ web/account_created.php
@@ -14,22 +14,22 @@ require "dbConnect.php";
 $db = get_db();
 
 if ($_SERVER[REQUEST_METHOD] == 'POST') {
-    try {
-
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-        $hash = password_hash($password, PASSWORD_DEFAULT);
-        $query = "INSERT INTO users(username, password) VALUES('" . $username . "', '" . $hash . "')";
-        //$statement = $db->prepare($query);
-        $db->exec($query);
-    }
-    catch(PDOException $ex) {
-        //print "<p>error: $ex->getMessage() </p>\n\n";
-    }
+//    try {
+//
+//        $username = $_POST['username'];
+//        $password = $_POST['password'];
+//        $hash = password_hash($password, PASSWORD_DEFAULT);
+//        $query = "INSERT INTO users(username, password) VALUES('" . $username . "', '" . $hash . "')";
+//        //$statement = $db->prepare($query);
+//        $db->exec($query);
+//    }
+//    catch(PDOException $ex) {
+//        //print "<p>error: $ex->getMessage() </p>\n\n";
+//    }
 }
-
-if (isset($_POST['new_user'])) {
-    header("Location: login.php");
-    exit;
-}
+//
+//if (isset($_POST['new_user'])) {
+//    header("Location: login.php");
+//    exit;
+//}
 ?>
