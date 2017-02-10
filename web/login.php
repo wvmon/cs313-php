e4ed555ef557155d40365d2d2713afbeb1e4504c
William Montesedoca <wvmon360@gmail.com>
2017-02-09 22:57:13 -0700

    Now it should work

--- web/login.php
+++ web/login.php
@@ -22,18 +22,19 @@ if (isset($_SESSION['loggedin'])) {
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script>
+            function check_empty() {
+                if (document.getElementById('username').value == "" || document.getElementById('password').value == "" ) {
+                    //alert("Please fill out all fields.");
+                    document.getElementById("error").textContent= "Empty fields exist.";
+                    return false;
+                } else {
+                    document.getElementById('form').submit();
+                }
+            }
+        </script>
     </head>
     <body>
-        <?php
-        if (isset($_POST['login'])) {
-            if (empty($_POST['username'])) {
-                echo "Username is Required";
-            }
-            if (empty($_POST['password'])) {
-                echo "Password is Required";
-            }
-        }
-        ?>
         <div class="container">
             <div class="row">
                 <div class="col-lg-4"></div>
