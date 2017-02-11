bb3d2cebaf11b5c0ec8b2f912ce48d33ec6561d2
William Montesedoca <wvmon360@gmail.com>
2017-02-11 00:46:16 -0700

    Script conditioning

--- web/login.php
+++ web/login.php
@@ -22,13 +22,24 @@ if (isset($_SESSION['loggedin'])) {
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script>
+            function check_empty() {
+                if (document.getElementById('username').value == "" || document.getElementById('password').value == "" ) {
+                    //alert("Please fill out all fields.");
+                    document.getElementById("error").textContent= "Empty fields exist.";
+                    return false;
+                } else {
+                    document.getElementById('form').submit();
+                }
+            }
+        </script>
     </head>
     <body>
         <div class="container">
             <div class="row">
                 <div class="col-lg-4"></div>
                 <div class="col-lg-4">
-                    <form action="loggedin.php" id="form" method="POST">
+                    <form onsubmit="check_empty()" action="loggedin.php" id="form" method="POST">
                         <fieldset>
                             <legend class="legend">Login</legend>
                             <div class="form_stuff">
@@ -42,7 +53,7 @@ if (isset($_SESSION['loggedin'])) {
                             </div>                                
                         </fieldset>                    
                     </form>
-                    <span class="errorspan"><?php echo $_SESSION['error']; ?></span>
+                    <span id="error" class="errorspan"><?php echo $_SESSION['error']; ?></span>
                 </div>
                 <div class="col-lg-4"></div>
             </div>                   
