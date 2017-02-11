ca4276bb786b38a854149d219dab6400d9f8e287
William Montesedoca <wvmon360@gmail.com>
2017-02-11 01:12:26 -0700

    Please work

--- web/login.php
+++ web/login.php
@@ -22,24 +22,13 @@ if (isset($_SESSION['loggedin'])) {
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-        <script>
-            function check_empty() {
-                if (document.getElementById()'username').value == "" || document.getElementById('password').value == "" ) {
-                    //alert("Please fill out all fields.");
-                    document.getElementById("error").innerHTML = "Empty fields exist.";
-                } else {
-                    document.getElementById('form').submit();
-                }
-            }
-        </script>
     </head>
     <body>
         <div class="container">
             <div class="row">
                 <div class="col-lg-4"></div>
                 <div class="col-lg-4">
-                    <span id="error" class="errorspan"></span>
-                    <form onsubmit="return check_empty()" action="loggedin.php" id="form" method="POST">
+                    <form action="loggedin.php" id="form" method="POST">
                         <fieldset>
                             <legend class="legend">Login</legend>
                             <div class="form_stuff">
