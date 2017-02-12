2b6b281c73346f01371ef627cbc7c360f456a38a
William Montesedoca <wvmon360@gmail.com>
2017-02-12 00:46:41 -0700

    Login update

--- web/loggedin.php
+++ web/loggedin.php
@@ -47,7 +47,7 @@ $password = $_POST['password'];
                     $q->execute();
                     $results = $q->fetch(PDO::FETCH_ASSOC);
 
-                    if (count($results) > 0 && isset($password)) {
+                    if (count($results) > 0 && isset($q)) {
                         $_SESSION['loggedin'] = $username;
                     }
                 } else {
