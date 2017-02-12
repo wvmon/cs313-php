19375b93913df8c3d1e44f419691cfa96a1ded7c
William Montesedoca <wvmon360@gmail.com>
2017-02-11 17:03:09 -0700

    New continioning

--- web/loggedin.php
+++ web/loggedin.php
@@ -52,12 +52,12 @@ $password = $_POST['password'];
         echo '<h3 class="welcome">Welcome '. $_SESSION['loggedin'] . '!</h3>';
         
         // ACCESS DENIED!!
-        /*if (!isset($_SESSION['loggedin'])) {
+        if (!isset($_SESSION['loggedin'])) {
             $_SESSION['error'] = "Invalid Username or Password";
             header("Location: login.php");
             exit;
-        }*/
-        elseif (empty($_POST[$username] && empty($_POST[$password]))) {
+        }
+        elseif (empty($_SESSION['loggedin'])) {
             $_SESSION['error'] = "Required fields are empty";
             header("Location: login.php");
             exit;
