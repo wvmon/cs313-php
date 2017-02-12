92912767e1e0ccd2a85b85bad5008cc57164b62f
William Montesedoca <wvmon360@gmail.com>
2017-02-11 18:12:48 -0700

    New continioning

--- web/login.php
+++ web/login.php
@@ -9,7 +9,7 @@
 session_start();
 if (isset($_SESSION['loggedin'])) {
     header("Location: loggedin.php");
-    die();
+    exit;
 }
 ?>
 
@@ -23,24 +23,12 @@ if (isset($_SESSION['loggedin'])) {
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-        <script>
-            function check_empty() {
-                if (document.getElementById('username').value == "" || document.getElementById('password').value == "" ) {
-                    //alert("Please fill out all fields.");
-                    document.getElementById("error").textContent= "Empty fields exist.";
-                    return false;
-                } else {
-                    document.getElementById('form').submit();
-                }
-            }
-        </script>
     </head>
     <body>
         <div class="container">
             <div class="row">
                 <div class="col-lg-4"></div>
                 <div class="col-lg-4">
-                    <span id="error" class="errorspan"></span>
                     <form onsubmit="check_empty()" action="loggedin.php" id="form" method="POST">
                         <fieldset>
                             <legend class="legend">Login</legend>
