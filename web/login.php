60563fb956c588f3e6a717ff3ea138eba9e35a23
William Montesedoca <wvmon360@gmail.com>
2017-02-12 19:07:33 -0700

    New message

--- web/login.php
+++ web/login.php
@@ -43,7 +43,7 @@ if (isset($_SESSION['loggedin'])) {
                             </div>                                
                         </fieldset>                    
                     </form>
-                    <span class="errorspan"><?php echo $_SESSION['error']; ?></span>
+                    <?php echo $_SESSION['error']; ?>
                 </div>
                 <div class="col-lg-4"></div>
             </div>                   
