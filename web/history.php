1fc32175348ffd981b89e7b93928df074a7387c9
William Montesedoca <wvmon360@gmail.com>
2017-02-19 00:21:20 -0700

    Remove & Edit icon

--- web/history.php
+++ web/history.php
@@ -24,7 +24,8 @@ if ($_SESSION['user']) {
         $title = $row['title'];
         $date = $row['entry_date'];
 
-        echo "<div id='comment_box'><h3>$title</h3><p>$date</p><p>$id</p><a href='#' name='edit'>Edit</a><a href='#' name='delete'>Delete</a></div>";
+        echo "<div id='comment_box'><h3>$title</h3><p>$date</p><p>$id</p><a href='#' name='edit'><i class=\"fa fa-pencil\" 
+aria-hidden=\"true\"></i></a><a href='#' name='delete'><i class=\"fa fa-trash\" aria-hidden=\"true\"></i>/a></div>";
     }
 }
 ?>
