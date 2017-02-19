cb46a836346cd0aa0006a13a2ffb930f0b9ab804
William Montesedoca <wvmon360@gmail.com>
2017-02-18 17:46:38 -0700

    History code

--- web/history.php
+++ web/history.php
@@ -31,6 +31,12 @@ if ($_SESSION['user']) {
 <html>
 <head>
     <title>History</title>
+    <meta charset="utf-8">
+    <meta name="viewort" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="stylesheets/stylesheet.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
 <a href="loggedin.php">Homepage</a>
