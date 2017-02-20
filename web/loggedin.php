34e11c32b791fedca79a7a006280ec13c3432289
William Montesedoca <wvmon360@gmail.com>
2017-02-20 02:44:35 -0700

    Work!!!!!!!!!!

--- web/loggedin.php
+++ web/loggedin.php
@@ -88,9 +88,9 @@ $password = filter_var($_POST['password'], FILTER_SANITIZE_STRING, FILTER_FLAG_N
         ?>
         <div class="container">
             <div class="row logged">
-                <div class="col-lg-4 icon"><a href="new_entry.php"><i class="fa fa-file fa-5x" aria-hidden="true"></i></a></div>
-                <div class="col-lg-4 icon"><a href="history.php"><i class="fa fa-history fa-5x" aria-hidden="true"></i></a></div>
-                <div class="col-lg-4 icon"><a href="logout.php"><i class="fa fa-sign-out fa-5x" aria-hidden="true"></i></a></div>
+                <div class="col-lg-4"><a href="new_entry.php" class="icon"><i class="fa fa-file fa-5x" aria-hidden="true"></i></a></div>
+                <div class="col-lg-4"><a href="history.php" class="icon"><i class="fa fa-history fa-5x" aria-hidden="true"></i></a></div>
+                <div class="col-lg-4"><a href="logout.php" class="icon"><i class="fa fa-sign-out fa-5x" aria-hidden="true"></i></a></div>
             </div>
         </div>
     </body>
