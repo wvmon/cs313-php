d43a400b4e331a760d160a33673941e77a593574
William Montesedoca <wvmon360@gmail.com>
2017-02-19 22:56:10 -0700

    Work!!!!!!!!!!

--- web/loggedin.php
+++ web/loggedin.php
@@ -21,6 +21,7 @@ $password = filter_var($_POST['password'], FILTER_SANITIZE_STRING, FILTER_FLAG_N
         <meta name="viewort" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="stylesheets/styling.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://use.fontawesome.com/b116fe7e2c.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     </head>
@@ -90,8 +91,8 @@ $password = filter_var($_POST['password'], FILTER_SANITIZE_STRING, FILTER_FLAG_N
             exit;
         }
         ?>
-        <a href="new_entry.php">New Entry</a>
-        <a href="history.php">View Entries</a>
-        <a href="logout.php">Logout</a>
+        <a href="new_entry.php"><h1><i class="fa fa-file" aria-hidden="true"></i></h1></a>
+        <a href="history.php"><h1><i class="fa fa-history" aria-hidden="true"></i></h1></a>
+        <a href="logout.php"><h1><i class="fa fa-sign-out" aria-hidden="true"></i></h1></a>
     </body>
 </html>
